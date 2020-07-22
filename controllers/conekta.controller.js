14df47e36859068a055d40f8ddca8ccc52491685
Marina <alcubilla.marina@gmail.com>
2020-07-22 15:04:25 -0500

    conekta

--- controllers/conekta.controller.js
+++ controllers/conekta.controller.js
@@ -1,20 +1,18 @@
 require('dotenv').config
 var conekta = require('conekta');
 const config = require('../config');
+const { subscribe } = require('../routers/conekta.router');
 const { conekta_key } = config.conekta;
 
-console.log("private", conekta_key);
+//console.log("private", conekta_key);
 
 
 
 module.exports = {
-
-
   create: function(req, res){
+    console.log("entro a crate")
     console.log("body", req.body)
-
     const { currency, type, nameItem, unit_price, quantity, name, phone, email, token_id } = req.body;
-
     const payload = {
       currency,
       customer_info: { name, phone, email },
@@ -35,10 +33,36 @@ module.exports = {
         console.log(detail);
         res.status(400).send(detail)
       }) 
+    }) 
 
-    })   
+  },
 
+  plan: function(req, res){
+
+    const { type, name, phone, email, token_id, nameItem } = req.body;    
+    const cliente = {
+      name,
+      email,
+      phone,
+      payment_sources: [{token_id, type}]
+    }
+
+    conekta.api_key = conekta_key;
+    conekta.api_version = '2.0.0';
+    conekta.locale = 'es'; 
+
+    conekta.Customer.create(cliente) 
+    .then(function (cliente) {
+      customer = conekta.Customer.find(cliente._id)
+      .then(function (customer) { 
+        customer.createSubscription({plan: nameItem})
+        .then(function(plan) {
+          console.log("res", plan)
+          res.send(plan)
+        })
+      })
+      //.catch(error => res.status(400).json({Error: 'No se encontro el usuario'}))
+    })
+    //.catch(error => res.status(400).json({ Error: 'No se pudo crear el usuario' }))
   }
-
-
 }
--- routers/conekta.router.js
+++ routers/conekta.router.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const Orden = require('../controllers/conekta.controller')
 
 router.route('/charge').post(Orden.create)
+router.route('/plan').post(Orden.plan)
 
 module.exports = router;
